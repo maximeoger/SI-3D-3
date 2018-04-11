4fb42801d38960929b041a50f51c8f822feea55f
SophieChieko <smemeint@gmail.com>
2018-04-11 20:00:01 +0200

    Sophie

---
+++ includes/accueil.php
@@ -0,0 +1,112 @@
+<section class="Section1">
+    	<img class="Section1-image" src="img/main.png">
+    </section>
+    <section class="Section2">
+    	<div class="Section2-Thumb">
+    		<img class="Section2-Thumb-BlackSwan" src="img/blackswan.png">
+    		<p class="Section2-Thumb-Text">Black Swan</p>
+            <div class="Section2-Thumb-traitrose"></div>
+    		<div class="Section2-Thumb-EuroFleche">
+    			<span class="Section2-Thumb-EuroFleche-Euro">115 EUR</span>
+				<i class="fas fa-arrow-right"></i>
+    		</div>
+    	</div>
+        <div class="Section2-Main"></div>
+        <img class="Section2-MainImg" src="img/grosse.png">
+        <div class="Section2-MainText">
+            <p class="Section2-MainText-Haut">CREATION D'AVRIL</p>
+            <p class="Section2-MainText-Bas">élue avec plus de 289 votes</p>
+        </div>
+        <div class="Section2-WhiteSquare"></div>
+        <div class="Section2-Thumb2">
+            <img class="Section2-Thumb2-BlackSwan" src="img/letrio.png">
+            <p class="Section2-Thumb2-Text">Le trio</p>
+            <div class="Section2-Thumb-traitrose"></div>
+            <div class="Section2-Thumb2-EuroFleche">
+                <span class="Section2-Thumb2-EuroFleche-Euro">229 EUR</span>
+                <i class="fas fa-arrow-right"></i>
+            </div>
+        </div>
+        <div class="Section2-Thumb3">
+            <img class="Section2-Thumb3-BlackSwan" src="img/clelia.png">
+            <p class="Section2-Thumb3-Text">Clélia</p>
+            <div class="Section2-Thumb-traitrose"></div>
+            <div class="Section2-Thumb3-EuroFleche">
+                <span class="Section2-Thumb3-EuroFleche-Euro">229 EUR</span>
+                <i class="fas fa-arrow-right"></i>
+            </div>
+        </div>
+        <img class="Section2-Femme" src="img/femme.png">
+
+    </section>
+    <section class="Section3">
+        <p class="Section3-decouvrez">DECOUVREZ LE CONFIGURATEUR 3D BY SOKARIS</p>
+        <div class="Section3-ImgText">
+            <img class="Section3-bague" src="img/bague.png">
+            <div class="Section3-Text">
+                <div class="Section3-traitrose"></div>
+                <p class="Section3-Text-Personnalisez">Personnalisez une bague qui vous ressemble !</p>
+                <div class="Section3-Text-Personnalisez-div">
+                    <i class="Section3-Text-icon fas fa-arrow-right"></i>
+                    <span class="Section3-Text-Creez">CREER MON BIJOU</span>
+                    </div>
+                
+            </div>
+            
+        </div>
+        
+    </section>
+    <section class="Section4">
+        <div class="Section4-div">
+            <img src="">
+            <span>Newsletter mensuelle</span>
+        </div>
+        <div class="Section4-div">
+            <img src="">
+            <span>Newsletter mensuelle</span>
+        </div>
+        <div class="Section4-div">
+            <img src="">
+            <span>Newsletter mensuelle</span>
+        </div>
+        <div class="Section4-div">
+            <img src="">
+            <span>Newsletter mensuelle</span>
+        </div>
+    </section>
+
+    <section class="Section5">
+        <div class="Section5-Top">
+            <h1 class="Section5-Title">sokaris</h1>
+            <img src="">
+            <div class="Section5-Services">
+                <p class="Section5-Services-Bold">Services</p>
+                <p class="Section5-Services-Light">Achats</p>
+                <p class="Section5-Services-Light">Paiements</p>
+                <p class="Section5-Services-Light">Comment ça marche</p>
+            </div>
+            <div class="Section5-Apropos">
+                <p class="Section5-Apropos-Bold">A propos</p>
+                <p class="Section5-Apropos-Light">Qui sommes-nous</p>
+                <p class="Section5-Apropos-Light">Contactez-nous</p>
+                <p class="Section5-Apropos-Light">FAQ</p>
+                <p class="Section5-Apropos-Light">Mentions légales</p>
+            </div>
+            <div class="Rejoignez">
+                <p class="Section5-Rejoignez-Bold">REJOIGNEZ-NOUS</p>
+                <p class="Section5-Rejoignez-Light">Newsletter mensuelle</p>
+                <input class="Section5-Rejoignez-Input" type="text" name="" placeholder="       prenom.nom@domaine.com">
+                <button class="Section5-Rejoignez-Button">S'INSCRIRE</button>
+            </div>
+        </div>
+        <div class="Section5-Bottom">
+            <span>&copy; 2018</span>
+            <div class="Section5-Bottom-Social">
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-google-plus-g"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-twitter"></i>
+            </div>
+        </div>
+        
+    </section>
