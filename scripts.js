abdc2df662792e346fd8ffdff2997495638e9a17
Brahim <brahim.mahious@yahoo.fr>
2018-04-11 14:37:06 +0200

    js update

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 var tunnel_pages = document.querySelectorAll('.Tunnel-pages');
-var buttonsRight = document.querySelectorAll('.Tunnel-pages-buttonRight');
-var buttonsLeft = document.querySelectorAll('.Tunnel-pages-buttonLeft');
+var buttonsRight = document.querySelectorAll('.Tunnel-pages-button-Right');
+var buttonsLeft = document.querySelectorAll('.Tunnel-pages-button-Left');
+var inputs = document.querySelectorAll('input');
  var index = 0;
+
+
+
   for (var i = 0; i < buttonsRight.length; i++) {
     buttonsRight[i].addEventListener('click', function() {
       console.log('hiii');
-    tunnel_pages[index].classList.remove('is-active');
-    index++;
-    tunnel_pages[index].classList.add('is-active');
+      tunnel_pages[index].classList.remove('is-active');
+      index++;
+      tunnel_pages[index].classList.add('is-active');
+
+      for (var i = 0; i < inputs.length; i++) {
+        var champs = inputs[i].value;
+        if ( champs === '') {
+          stopPropagation();
+        }
+      }
+
   });
   }
   for (var i = 0; i < buttonsLeft.length; i++) {
