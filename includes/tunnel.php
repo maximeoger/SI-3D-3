e540b2503fd21ea3f53a5d855f6639b032ffbb6b
maximeoger <maximeoger93@gmail.com>
2018-04-11 14:18:29 +0200

    ajout tunnel de conversion

--- includes/tunnel.php
+++ includes/tunnel.php
@@ -1,50 +1,63 @@
 
 <div class="Tunnel">
+<div class="Tunnel-container">
 
- <div class="Tunnel-pages is-active">
+    <div class="Tunnel-overlay"></div>
 
-      <h1>Entrez vos coordonées</h1>
-       <label for="prénom"> Entrez votre prénom:</label>
-       <input type="text" name="prénom" value=""><br>
-       <label for="prénom"> Entrez votre Nom:</label>
-       <input type="text" name="Nom" value=""><br>
-       <label for="prénom"> Entrez votre E-mail:</label>
-       <input type="email" name="mail" value=""><br>
-       <label for="prénom"> Entrez votre numéro de téléphone:</label>
-       <input type="tel" name="tel" value=""><br>
+    <div class="Tunnel-pagesContainer">
 
-   <button class="button1"type="button" > passer à l'étape suivante </button>
+        <div class="Tunnel-pages is-active">
 
- </div>
+            <h1 class="Tunnel-pagesTitle">Entrez vos coordonées</h1>
 
- <div class="Tunnel-pages">
-   <h1>Rensegner votre adresse</h1>
-   <label for="prénom"> Entrez votre adresse:</label>
-   <input type="text" name="prénom" value=""><br>
-   <label for="prénom"> rue:</label>
-   <input type="text" name="Nom" value=""><br>
-   <label for="prénom"> code postale:</label>
-   <input type="email" name="mail" value=""><br>
-   <label for="prénom"> lebelé de la voie:</label>
-   <input type="tel" name="tel" value=""><br>
+            <label for="prénom"> Entrez votre prénom:</label>
+            <input type="text" name="prénom" value=""><br>
+            <label for="prénom"> Entrez votre Nom:</label>
+            <input type="text" name="Nom" value=""><br>
+            <label for="prénom"> Entrez votre E-mail:</label>
+            <input type="email" name="mail" value=""><br>
+            <label for="prénom"> Entrez votre numéro de téléphone:</label>
+            <input type="tel" name="tel" value=""><br>
 
-   <button class="button1"type="button" > passer à l'étape suivante </button>
-   <button class="button2"type="button" > revenir </button>
- </div>
+            <button class="button1"type="button" > passer à l'étape suivante </button>
 
- <div class="Tunnel-pages">
-   <h1>soumettre le paiement</h1>
-   <label for="prénom"> Numéro de la carte</label>
-   <input type="text" name="prénom" value=""><br>
-   <label for="prénom"> échéance</label>
-   <input type="text" name="Nom" value=""><br>
-   <label for="prénom"> cryotogramme</label>
-   <input type="text" name="Nom" value=""><br>
+        </div>
+        <div class="Tunnel-pages">
+            <h1 class="Tunnel-pagesTitle">Rensegner votre adresse</h1>
+            <label for="prénom"> Entrez votre adresse:</label>
+            <input type="text" name="prénom" value=""><br>
+            <label for="prénom"> rue:</label>
+            <input type="text" name="Nom" value=""><br>
+            <label for="prénom"> code postale:</label>
+            <input type="email" name="mail" value=""><br>
+            <label for="prénom"> lebelé de la voie:</label>
+            <input type="tel" name="tel" value=""><br>
 
-   <button class="Tunnel-buttonRight"type="button" > passer le paiement</button>
-   <button class="Tunnel-buttonLeft"type="button" > revenir </button>
- </div>
+            <button class="button1"type="button" > passer à l'étape suivante </button>
+            <button class="button2"type="button" > revenir </button>
+        </div>
 
- <div class="Tunnel-pages">
-      <h1>Félicitation</h1>
- </div>
+        <div class="Tunnel-pages">
+            <h1 class="Tunnel-pagesTitle">soumettre le paiement</h1>
+            <label for="prénom"> Numéro de la carte</label>
+            <input type="text" name="prénom" value=""><br>
+            <label for="prénom"> échéance</label>
+            <input type="text" name="Nom" value=""><br>
+            <label for="prénom"> cryotogramme</label>
+            <input type="text" name="Nom" value=""><br>
+
+            <button class="Tunnel-buttonRight"type="button" > passer le paiement</button>
+            <button class="Tunnel-buttonLeft"type="button" > revenir </button>
+        </div>
+
+        <div class="Tunnel-pages">
+            <h1 class="Tunnel-pagesTitle">Félicitation</h1>
+        </div>
+    </div>
+
+</div>
+</div>
+
+
+
+
